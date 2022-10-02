7189c4de4f48294aa90228fbb43f1bd7e24ffe1b
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2022-10-03 00:04:39 +0530

    Update Count_inversion.cpp

--- Count_inversion.cpp
+++ Count_inversion.cpp
@@ -1,6 +1,5 @@
 /* :)
 Problem Statement: Given an array of integers. Find the Inversion Count in the array.
-
 Inversion Count: For an array, inversion count indicates how far (or close) the array is from
 being sorted. If array is already sorted then the inversion count is 0. If an array is sorted in the
 reverse order then the inversion count is the maximum.
@@ -65,9 +64,7 @@ int merge(int arr[],int low,int mid,int high){
   for (i = low; i <= high; i++) {
     arr[i] = temp[m];
     m++;
-  }
- 
-    
+  }    
   return count;  
 }
 
